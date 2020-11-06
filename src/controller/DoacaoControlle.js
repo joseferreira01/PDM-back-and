31c5f05e14199655d5182ef638f6d3507b0c9b40
josé <joseifpb2015@gmail.com>
2020-11-05 21:14:02 -0300

    controler doaçao criado

---
+++ src/controller/DoacaoControlle.js
@@ -0,0 +1,59 @@
+const conection = require('../database/conection');
+const bcrypt = require('bcrypt');
+
+
+module.exports = {
+  
+     
+    async index(request, response, next){ 
+    //  return  response.json({nome:'jose'});
+        const {page = 1} = request.query;
+        const [count] = await conection('doacao').count();
+        
+       const message = await conection('doacao').limit(10).
+       offset((page -1)*10).
+       select('id','email_ong', 'valor');
+       response.header('X-Total-Count',count['count(*)']);
+        response.json({message})
+    },
+    async findOne(request, response,next){
+        const {id} =  request.params;
+        const message = await conection('user').
+        where('id',id).select('id','login');
+         response.json({message})
+     },
+
+    async create(request, response, next){
+     
+        const {valor,email_ong, email_usuario } = request.body;
+      
+        const password = bcrypt.hashSync(request.body.password, 10)
+
+      const result = await conection('doacao').insert({
+          valor,
+          email_ong
+        }).then(message =>{
+          return  response.status(200).json({success:'success'});
+        }).catch(err =>{
+          return  response.status(201).json({error:err});
+        });
+   
+     
+    },
+    async delete(request, response, next){
+       
+            const id = request.params;
+      
+            await conection('doacao').where('id',id)
+            .delete().then(message => {
+
+              return  response.status(200).json({success:'success'});
+
+            }).catch(err =>{
+              return  response.status(404).json({err:err});
+            });
+       
+          
+    },
+    
+};

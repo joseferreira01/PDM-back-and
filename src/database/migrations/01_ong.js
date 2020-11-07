1e8fd4145fa2aed952ae7028842b0e3ce62d6ca0
LuzEsperanza <luzdeesperanzaapolo@gmail.com>
2020-11-07 11:04:55 -0300

    modifiancando ong

--- src/database/migrations/01_ong.js
+++ src/database/migrations/01_ong.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('ong', function (table) {
         table.string('name').notNullable();
         table.string('email').primary();
+        table.string('senha').notNullable();
         table.string('telefone').notNullable();
         table.string('descricao');
         table.string('uf').notNullable();

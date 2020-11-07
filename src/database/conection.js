1e5f9073aeeddc64f64c0aca8eeb8e48f7fb3035
jussara <aras.strong8@gmail.com>
2020-11-06 21:15:58 -0300

    cliente em connection

--- src/database/conection.js
+++ src/database/conection.js
@@ -1,5 +1,14 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
+const path = require('path')
 const conection = knex(configuration.production);
+
+const connection = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'dev.sqlite3')
+    },
+    useNullAsDefault: true,
+})
 
 module.exports = conection;

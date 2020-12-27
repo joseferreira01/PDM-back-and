5de177b22676218d8b0d95dc69a609a49c817ece
josé <joseifpb2015@gmail.com>
2020-12-26 23:36:15 -0300

    up image finalizado

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.use('uploads',express.static(path.join(__dirname,'..','uploads')))
+app.use('/uploads',express.static(path.join(__dirname,'..','uploads')))
 // para carregar arquivos
 //app.use (bodyParser.json ({limit: '10mb', extended: true}))
 
